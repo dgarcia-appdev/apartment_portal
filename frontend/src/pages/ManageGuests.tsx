822f729c0847e6da7886b453f133bc57beb3b3b4
Ayomide Omotosho <aomotosho4@gmail.com>
2025-03-08 14:39:43 -0600

    Add ManageGuests page

---
+++ frontend/src/pages/ManageGuests.tsx
@@ -0,0 +1,6 @@
+export default function ManageGuests() {
+  return (
+    <div className="m-0 p-0 w-screen min-h-screen bg-accent">
+      <h1 className=" text-4xl font-heading">Hello World</h1>
+    </div>)
+}
